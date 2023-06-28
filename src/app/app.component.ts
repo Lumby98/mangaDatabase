9d75cfe36452258539b4dcafaa2e980b31ba8920
Michael <lumby98@gmail.com>
2023-06-28 22:27:48 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `<div>{{message}}</div>`,
 })
 export class AppComponent {
-  value = 'World';
+  message = '';
+
+  constructor(private http: HttpClient) {
+    this.http.get('/api/WaifuGet')
+      .subscribe((resp: any) => this.message = resp.text);
+  }
 }
